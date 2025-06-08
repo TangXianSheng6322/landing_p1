f05a7594a6b5eb8f6181bb526c90f60fc2418765
Nick <n.sakharov@icloud.com>
2025-06-08 18:34:52 +0700

    Finished navbar

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
-export class HeroComponent {
-
-}
+export class HeroComponent {}
