076ee655acc94675537c46f2ec6b0cceeb1cf6ff
Nick <n.sakharov@icloud.com>
2025-06-14 22:22:28 +0700

    Move navbar component out so it will be visible throughout website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { Hero2Component } from './hero2/hero2.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent, Hero2Component],
+  imports: [
+    RouterOutlet,
+    HeroComponent,
+    Hero2Component,
+    BenefitsComponent,
+    NavbarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [NavbarComponent],
+  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
