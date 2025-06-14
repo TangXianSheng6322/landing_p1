a720c1e9c0e261cac67eb9b815658703e70e274e
Nick <n.sakharov@icloud.com>
2025-06-14 21:11:28 +0700

    Finished second section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
+import { Hero2Component } from './hero2/hero2.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent],
+  imports: [RouterOutlet, HeroComponent, Hero2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
